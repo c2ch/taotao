18e71ec2f47e41d4256010c1be07e64769901e21
c2ch <1138366504@qq.com>
2018-01-02 21:31:04 +0800

    创建了一个person类

---
+++ taotao/taotao-common/src/main/java/com/c2c/pojo/person.java
@@ -0,0 +1,44 @@
+package com.c2c.pojo;
+
+/**
+ * Copyright: Copyright (c) 2018 linewell
+ *
+ * @ClassName: person
+ * @Description: 描述人的一个类
+ * @version: v1.0.0
+ * @author: cc
+ * @date: 2018/1/2 21:29
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ---------------------------------------------------------*
+ */
+public class person {
+    private String name;
+    private Integer age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
