83d7d3bbf5f0b2ff718cdbb4d84a8c85d59bd841
c2ch <1138366504@qq.com>
2018-01-03 17:52:06 +0800

    创建了mybatis逆向工程

---
+++ generator-mybatis/src/main/java/com/c2c/mapper/TbItemMapper.java
@@ -0,0 +1,30 @@
+package com.c2c.mapper;
+
+import com.c2c.pojo.TbItem;
+import com.c2c.pojo.TbItemExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbItemMapper {
+    long countByExample(TbItemExample example);
+
+    int deleteByExample(TbItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItem record);
+
+    int insertSelective(TbItem record);
+
+    List<TbItem> selectByExample(TbItemExample example);
+
+    TbItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByExample(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByPrimaryKeySelective(TbItem record);
+
+    int updateByPrimaryKey(TbItem record);
+}
